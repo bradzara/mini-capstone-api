615e8aabda3d7e8d24da14c9c5113fc85ed0e2d4
bradzara <bradzara93@gmail.com>
2024-05-20 11:46:49 -0700

    Change description data type to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_183243) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_184510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_183243) do
     t.string "name"
     t.decimal "price", precision: 7, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventory"
